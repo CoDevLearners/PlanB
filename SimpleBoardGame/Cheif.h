f18bbb1ad1b279ad4cdc3f9bc52b6682fdea17e5
pkk1113_desktop <pkk1113@naver.com>
2021-01-05 23:27:23 +0900

    SimpleProject 삭제

--- SimpleBoardGame/Cheif.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include "PieceBase.h"
-class Cheif : public PieceBase
-{
-public:
-	explicit Cheif(Cell cell);
-	std::vector<Actions> GetBehavior(const Board& board) const override;
-};
-
